bcb7d39cba6a6305a549c0f89e9f30f1cc3c6ddc
Maxim Dobryakov <closer.main@gmail.com>
2012-07-05 19:47:11 +0400

    Initial Commit

---
+++ InAppPurchase/InAppPurchase.m
@@ -0,0 +1,13 @@
+//
+//  InAppPurchase.m
+//  InAppPurchase
+//
+//  Created by Максим Добряков on 7/5/12.
+//  Copyright (c) 2012 Studio Mobile. All rights reserved.
+//
+
+#import "InAppPurchase.h"
+
+@implementation InAppPurchase
+
+@end
